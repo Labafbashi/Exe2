1a63ce057038b006db79e876dae276c5cb7a6a5e
MohammadHossein Labafbashi <92016155+Labafbashi@users.noreply.github.com>
2022-11-07 11:49:54 +0100

    Add codes for menu 1 with decoration

--- Program1/Program.cs
+++ Program1/Program.cs
@@ -1,8 +1,12 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Security.Cryptography;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int userInput;
+        int userInput=5;
         do
         {
             DrawMenu();
@@ -14,12 +18,11 @@
             catch
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("*****************"); 
-                Console.WriteLine("Wrong Value.....");
-                Console.WriteLine("Please enter a number between [0-3]";
+                WriteError("***************** \n Wrong Value..... \n Please enter a number between [0-3] \n");
                 Console.WriteLine("Press any key to continue.");
                 Console.ReadKey();
-                return;
+                Console.ForegroundColor = ConsoleColor.White;
+                //return;
             }
             
 
@@ -29,7 +32,7 @@
                     Environment.Exit(0);
                     break;
                 case 1:
-
+                    OldYoung();
                     break;
                 case 2:
                     break;
@@ -39,6 +42,53 @@
                     break;
             }
         } while (true);
+    }
+
+    private static void OldYoung()
+    {
+        int age, guest = 0;
+        double price = 0;
+        Console.WriteLine("How many guest do you have?");
+        guest = Int32.Parse(Console.ReadLine());
+        for(int i = 0; i < guest; i++)
+        {
+            Console.WriteLine($"Please enter an age, Person {i+1}:");
+            age = Int32.Parse(Console.ReadLine());
+            if (age > 0 && age <= 120)
+            {
+                price = price + CheckAge(age);
+            }
+            else
+            {
+                i--;
+                WriteError("Wrong Value...");
+            }
+        }
+        WriteMessage($"Total Value for {guest} guest is {price}", ConsoleColor.Cyan);
+        Console.ReadKey();
+    }
+
+    private static void WriteError(string error)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(error);
+        Console.ForegroundColor = ConsoleColor.White;
+
+    }
+
+    private static void WriteMessage(string message, ConsoleColor c)
+    {
+        Console.ForegroundColor = c;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static int CheckAge(int age)
+    {
+        if (age < 20) { WriteMessage(">>>>> Ungdomspris: 80kr <<<<<", ConsoleColor.Yellow); return 80; }
+        else if (age > 20 && age < 60) { WriteMessage(">>>>> Standardpris: 120kr <<<<<", ConsoleColor.Yellow); return 120; }
+        else if (age > 60) { WriteMessage(">>>>> Pensionärspris: 90kr <<<<<", ConsoleColor.Yellow); return 90; }
+        else return 0;
     }
 
     private static void DrawMenu()
